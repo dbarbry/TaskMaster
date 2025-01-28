4d0b2eeac3133e1d48c9f14c7b6207af74f0dd41
rgeral <geral.robin@yahoo.fr>
2025-01-28 18:15:30 +0100

    adding getters in the ProgramConfig class

--- daemon/incs/parsing.hpp
+++ daemon/incs/parsing.hpp
@@ -25,23 +25,21 @@ public:
     std::string stderr_file;
     std::map<std::string, std::string> env;
 
-    void printConfig() const {
-        std::cout << "cmd: " << cmd << std::endl;
-        std::cout << "numprocs: " << numprocs << std::endl;
-        std::cout << "umask: " << umask << std::endl;
-        std::cout << "workingdir: " << workingdir << std::endl;
-        std::cout << "autostart: " << (autostart ? "true" : "false") << std::endl;
-        std::cout << "autorestart: " << autorestart << std::endl;
-        std::cout << "startretries: " << startretries << std::endl;
-        std::cout << "starttime: " << starttime << std::endl;
-        std::cout << "stopsignal: " << stopsignal << std::endl;
-        std::cout << "stoptime: " << stoptime << std::endl;
-        std::cout << "stdout: " << stdout_file << std::endl;
-        std::cout << "stderr: " << stderr_file << std::endl;
-        for (const auto& env_var : env) {
-            std::cout << "env: " << env_var.first << "=" << env_var.second << std::endl;
-        }
-    }
+    std::string getCmd() const { return cmd; }
+    int getNumprocs() const { return numprocs; }
+    std::string getUmask() const { return umask; }
+    std::string getWorkingDir() const { return workingdir; }
+    bool getAutostart() const { return autostart; }
+    std::string getAutorestart() const { return autorestart; }
+    const std::vector<int>& getExitcodes() const { return exitcodes; }
+    int getStartretries() const { return startretries; }
+    int getStarttime() const { return starttime; }
+    std::string getStopsignal() const { return stopsignal; }
+    int getStoptime() const { return stoptime; }
+    std::string getStdoutFile() const { return stdout_file; }
+    std::string getStderrFile() const { return stderr_file; }
+    const std::map<std::string, std::string>& getEnv() const { return env; }
+
 };
 
 void parsing(char **args);
