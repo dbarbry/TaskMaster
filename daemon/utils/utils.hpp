6179feac07ad3841f4ee002c3feaff5375d40ed1
kbarbry <kellianbarbry@gmail.com>
2025-02-21 14:38:06 +0100

    fix

--- daemon/utils/utils.hpp
+++ daemon/utils/utils.hpp
@@ -16,6 +16,7 @@
 std::string handle_cmd(std::string cmd, int server_fd, int client_fd,
                        std::map<std::string, std::vector<std::string>> parsedCommand,
                        std::map<std::string, ProgramConfig>            programs);
-bool        isAlreadyRunning(const std::string &programPath);
+
+bool isAlreadyRunning(const std::string &programPath);
 
 #endif
